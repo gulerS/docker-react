98d0eea72314345b1ccd52ec0094300225ad65d4
Sercan <sercanguler@live.com>
2020-09-20 02:26:48 +0300

    first commitx4

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">wow to React</h1>
+          <h1 className="App-title">React</h1>
         </header>
-        <p className="App-intro">Woah Whoop</p>
+        <p className="App-intro">Hi there!</p>
       </div>
     );
   }
